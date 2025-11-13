917aa6ab6ffbfcf72dfdd07d1a9bc351c7e087d4
Sakshi <sakshiborkar90@gmail.com>
2025-11-13 23:22:34 +0530

    Fix wildcard route for Render deployment

--- backend/server.js
+++ backend/server.js
@@ -25,9 +25,10 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-    app.get("/*", (req,res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
-    });
+    app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+
 }
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
