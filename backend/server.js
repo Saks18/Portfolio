b3742c9813d1d002451b57ad48ed7e92d84aee19
Sakshi <sakshiborkar90@gmail.com>
2025-11-13 21:26:14 +0530

    Added production code

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import projectRoutes from "./routes/projectRoutes.js";
+import path from "path";
+import {fileURLToPath} from "url";
 
 dotenv.config();
 
@@ -16,6 +18,16 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/projects", projectRoutes);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+//Serve frontend build files in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+    });
+}
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
